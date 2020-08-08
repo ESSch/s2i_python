5032728c394ff9fdb289964c5029c6a88b5db629
Evgenya Shtolts <ESSchtolts@yandex.ru>
2020-08-09 02:23:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import http.server
 import socketserver
-PORT = 8000
+PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
